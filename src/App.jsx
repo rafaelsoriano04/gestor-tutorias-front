145d49de91c770159107039cad06006670c20ac2
FreddyA12 <2balvarezfreddy@gmail.com>
2024-05-22 00:05:03 -0500

    Estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import { useState } from 'react'
+import './App.css';
+import { useState } from 'react';
 import imgInicio from "./assets/inicio.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div class="container text-center">
-      <div class="row align-items-center">
-        <div class="col d-flex align-items-center justify-content-center">
-          <img class="img" src={imgInicio} alt="" />
+    <div className="container text-center">
+      <div className="row align-items-center">
+        <div className="col d-flex align-items-center justify-content-center">
+          <img className="img" src={imgInicio} alt="Descripción de imagen" />
         </div>
-        <div class="col">
-          One of three columns
+        <div className="col">
+        
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
