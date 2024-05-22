0fcafaf6543b5f2ecc5b2ee5f8d7cbb89897e171
nanobotrs <raansopro324@gmail.com>
2024-05-22 12:44:04 -0500

    Cambio en metodo de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState } from "react";
 import imgInicio from "./assets/inicio.png";
-import Login from './components/Login.jsx';
-import Register from './components/Register.jsx';
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
 
 function App() {
   // Agregar la definición de estado para controlar la visualización de Login o Register
-  const [showLogin, setShowLogin] = useState(true);  // Esto faltaba en tu versión
+  const [showLogin, setShowLogin] = useState(true);
+
+  const handelClick = () => {
+    if (showLogin) return <Login onRegisterClick={() => setShowLogin(false)} />;
+    return <Register onLoginClick={() => setShowLogin(true)} />;
+  };
 
   return (
     <div className="container text-center">
@@ -15,13 +20,7 @@ function App() {
         <div className="col d-flex align-items-center justify-content-center">
           <img className="img" src={imgInicio} alt="Inicio" />
         </div>
-        <div className="col">
-          {showLogin ? (
-            <Login onRegisterClick={() => setShowLogin(false)} />
-          ) : (
-            <Register onLoginClick={() => setShowLogin(true)} />
-          )}
-        </div>
+        <div className="col">{handelClick()}</div>
       </div>
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,79 +1,79 @@
 // src/components/Login.jsx
-import React, { useState } from 'react';
-import './Login.css';  // Asegúrate de que el path del CSS es correcto
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { ChangeEvent, useState } from "react";
+import axios from "axios";
+import "./Login.css"; // Asegúrate de que el path del CSS es correcto
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Login({ onRegisterClick }) {
-  const [formData, setFormData] = useState({
-    username: '',  // Cambiado de nombre_usuario a username
-    password: ''   // Cambiado de contrasenia a password
-  });
+  const [jwtToken, setJwtToken] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      [name]: value
-    }));
+  const validarDatos = async () => {
+    return await axios
+      .post("http://localhost:3000/auth/login", {
+        nombre_usuario: username,
+        contrasenia: password,
+      })
+      .then((resp) => resp.data)
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
   };
-  
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const response = await fetch('http://localhost:3000/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          nombre_usuario: formData.username,  
-          contrasenia: formData.password      
-        })
-      });
-      console.log("Raw Response:", response);
-      const data = await response.json();
-      if (response.ok) {
-        console.log('Login Successful:', data);
-        localStorage.setItem('token', data.token); // Guarda el token en localStorage
-        alert('Login successful!');
-      } else {
-        throw new Error(data.message);
-      }
-    } catch (error) {
-      console.error('Login failed:', error);
-      alert('Login failed: ' + error.message);
-    }
+    const token = await validarDatos();
+    console.log(token);
+    setJwtToken(token);
   };
-  
 
   return (
     <div>
       <form onSubmit={handleSubmit} className="login-form">
         <h2>Login</h2>
         <div className="form-group">
-          <label>Usuario:
+          <label>
+            Usuario:
             <input
               type="text"
               name="username"
               className="form-control"
-              value={formData.username}
-              onChange={handleChange}
+              value={username}
+              onChange={handleUsernameChange}
             />
           </label>
         </div>
         <div className="form-group">
-          <label>Contraseña:
+          <label>
+            Contraseña:
             <input
               type="password"
               name="password"
               className="form-control"
-              value={formData.password}
-              onChange={handleChange}
+              value={password}
+              onChange={handlePasswordChange}
             />
           </label>
         </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-        <button type="button" className="btn btn-link" onClick={onRegisterClick}>Register</button>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+        <button
+          type="button"
+          className="btn btn-link"
+          onClick={onRegisterClick}
+        >
+          Register
+        </button>
       </form>
     </div>
   );
