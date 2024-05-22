f305b9e0661c28e9f7ee98ecb9c03bc9a5222623
nanobotrs <raansopro324@gmail.com>
2024-05-21 22:12:58 -0500

    Estructura basica de componentes del login

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { useState } from 'react'
+import imgInicio from "./assets/inicio.png";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="container text-center">
+      <div class="row align-items-center">
+        <div class="col d-flex align-items-center justify-content-center">
+          <img class="img" src={imgInicio} alt="" />
+        </div>
+        <div class="col">
+          One of three columns
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
